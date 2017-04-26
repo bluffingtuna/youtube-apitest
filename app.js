b376e1fc9ab6b4f64d8973e540c37be3004be890
Jake Jinsuk Park <bluffingtuna@gmail.com>
2017-04-26 15:28:08 -0700

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ $(function() {
 		request.execute(function(response) {
 			var results = response.result;
 			$.each(results.items, function(index, item) {
-				console.log(item);
+				$.get("tpl/item.html", function(data) {
+					$("#result").append(data);
+				})
 			})
 		})
 	});
