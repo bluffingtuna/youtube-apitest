201913d7c1a81cf3452f166548dbcd71583564fc
bluffingtuna@gmail.com <bluffingtuna@gmail.com>
2017-04-27 10:37:06 -0700

    files added

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 $(function() {
-	$("form").on("submit", function(e) {
-		e.preventDefault();
-		// prepare the request
-		var request = gapi.client.youtube.search.list({
-			part: "snippet",
-			type: "video",
-			q: encodeURIComponent($("#search").val()).replace(/%20/g,"+"),
-			maxResults: 3,
-			order: "viewCount",
-			publishedAfter: "2015-01-01T00:00:00Z"
-		});
-		//execute the request
-		request.execute(function(response) {
-			var results = response.result;
-			$.each(results.items, function(index, item) {
-				console.log(item);
-			})
-		})
-	});
+    $(document).on("submit", function(e) {
+        e.preventDefault();
+
+        var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 3,
+            order: "viewCount",
+            publishedAfter: "2015-01-01T00:00:00Z"
+        });
+
+        request.execute(function(response) {
+            var results = response.result;
+            $.each(results.items, function(index, item) {
+                $.get("tpl/item.html", function(data) {
+                    $("#results").append(tplawesome(data, [{ "title": item.snippet.title, "videoid": item.id.videoId }]));
+                });
+            });
+        })
+    });
 });
 
 function init() {
-	gapi.client.setApiKey("AIzaSyDR8HyLqczOk_klhd_wZASuOlyEr-nsUfM");
-	gapi.client.load("youtube", "v3", function(){
+    gapi.client.setApiKey("AIzaSyDR8HyLqczOk_klhd_wZASuOlyEr-nsUfM");
+    gapi.client.load("youtube", "v3", function() {
 
-	});
+    });
 }
